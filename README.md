06f4cecae81d5d64e747b35310fe474e45d5b9a5
suziwen <suziwen1@gmail.com>
2020-04-09 21:02:06 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# gatsby-action-sculpting
+
+A minimalist action for
+[gatsby-theme-sculpting](https://github.com/suziwen/gatsby-theme-sculpting).
+
+
+![screenshot](https://github.com/suziwen/gatsby-theme-sculpting/blob/master/screenshot.png)
+
+
+## Usage
+
+1. fork this repo
+2. upload zip files at `posts` fold.
+3. visit http://{yourname}.github.io/gatsby-action-sculpting
---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
+const siteMetadata = {
+  title: "小书匠",
+  siteUrl: "http://www.xiaoshujiang.com",
+  author: "suziwen",
+  description: `一款让你爱不释手的写作软件`,
+}
+module.exports = {
+  siteMetadata,
+  pathPrefix: "/gatsby-action-sculpting",
+  plugins: [{ 
+    resolve: `@suziwen/gatsby-theme-sculpting`,
+    options: {
+      contentPath: `posts`,
+      _disqus: {
+        shortname: process.env.DISQUS_SHORT_NAME
+      },
+      _gitalk: {
+        clientID: process.env.GITALK_CLIENT_ID,
+        clientSecret: process.env.GITALK_CLIENT_SECRET,
+        repo: process.env.GITALK_REPO,
+        owner: process.env.GITALK_OWNER,
+        admin: [process.env.GITALK_ADMIN],
+      }
+    } 
+  }],
+}
